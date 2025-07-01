d3f3e7d021a9a2b55203162b73e84c4c406bf96f
Matias Viola <matiasvioladi@gmail.com>
2025-07-01 19:24:17 -0300

    tipos, de definiciones,...

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+type AnswerOption = {
+  correct: string;
+  distractors: string[];
+};
+
+type FillInSegment = [string, AnswerOption];
+
+export interface FillInTheBlankDefinition {
+  prompt: string;
+  segments: FillInSegment[];
+}
